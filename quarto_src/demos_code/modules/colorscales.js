6c23f652a403122d3014e9d7d6c632e70875bbef
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-12-13 20:37:46 +0000

    Built site for gh-pages

--- quarto_src/demos_code/modules/colorscales.js
+++
@@ -1,7 +0,0 @@
-import "../libs/chroma.min.js";
-
-export function gradient(minval, maxval, lowc, highc, value){
-    let f = chroma.scale([lowc, highc]).domain([minval,maxval]);
-    let rgb = f(value).rgb();
-    return [rgb[0],rgb[1],rgb[2],255];
-}  
